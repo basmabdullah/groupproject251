ed247874688c9b491e189e96f35cf5ee6fc48460
basma abdullah <babdullah0032@stu.kau.edu.sa>
2024-02-05 12:10:21 +0300

    simple print statement

---
+++ src/main/java/com/mycompany/groupproject251/Groupproject251.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.groupproject251;
+
+/**
+ *
+ * @author 2206160
+ */
+public class Groupproject251 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello my name is basma!");
+    }
+}
